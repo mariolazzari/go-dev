48784fef8630c29e40a47b187e6b23e9d500f585
Mario Lazzari <mario.lazzari@gmail.com>
2025-05-10 14:45:12 +0200

    Done chapter 6 lesson 2

--- 06_structs/1_structs/main.go
+++ 06_structs/1_structs/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type MyMap map[string]string
+
+type User struct {
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
+}
+
 func main() {
+	var myMap MyMap = MyMap{"mario": "Mario"}
+	fmt.Println(myMap)
+
+	var user User = User{"Mario", 50}
+	fmt.Println(user)
+
+	var maria User = User{
+		Name: "Maria",
+		Age:  78,
+	}
+	fmt.Println(maria)
+
+	enc := json.NewEncoder(os.Stdout)
+
+	if err := enc.Encode(maria); err != nil {
+		log.Fatal(err)
+	}
 
 }

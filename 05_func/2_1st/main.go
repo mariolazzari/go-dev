280280e6de9b2eb0695b2c7eafcb4d82c6dd0ba1
Mario Lazzari <mario.lazzari@gmail.com>
2025-05-10 09:49:52 +0200

    Done chapter 5 lesson 3

--- 05_func/2_1st/main.go
+++ 05_func/2_1st/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fn := func() string {
+		return "Hello"
+	}
+
+	hello := fn()
+	fmt.Println(hello)
+
+	sayHello(fn)
+
+	name := "Mario"
+	hi := func() {
+		fmt.Println("Hi", name)
+	}
+	hi()
+}
+
+func sayHello(f func() string) {
+	fmt.Println(f())
 }
